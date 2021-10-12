48bcee9ca69023f22ffb9f938e9beb5421668d42
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2021-10-12 18:15:30 +0700

    first commit

---
+++ src/main/java/com/bezkoder/spring/jdbc/oracle/SpringBootJdbctemplateOracleApplication.java
@@ -0,0 +1,13 @@
+package com.bezkoder.spring.jdbc.oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbctemplateOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbctemplateOracleApplication.class, args);
+	}
+
+}
